45ac81a069581bff94cf0a6dca59d3728ac6c2e7
jonas.x.persson <jonas.x.persson@26840750-e8b8-a994-9f98-e2764c8d6831>
2011-05-11 20:06:59 +0000

    refactoring

--- Stixter.Plexi/Stixter.Plexi/Stixter.Plexi/PlexiGame.cs
+++ Stixter.Plexi/Stixter.Plexi/Stixter.Plexi/PlexiGame.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -22,13 +21,6 @@ namespace Stixter.Plexi
             Content.RootDirectory = "Content";
         }
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -38,9 +30,11 @@ namespace Stixter.Plexi
             _startScreen = new StartScreen(this, _spriteBatch, Content.Load<SpriteFont>("menufont"), Content.Load<Texture2D>("splash_screen"));
             Components.Add(_startScreen);
             _startScreen.Hide();
+            
             _actionScreen = new ActionScreen(this, _spriteBatch, Content.Load<Texture2D>("Levels\\level1_background"));
             Components.Add(_actionScreen);
             _actionScreen.Hide();
+
             _activeScreen = _startScreen;
             _activeScreen.Show();
         }
@@ -61,18 +55,13 @@ namespace Stixter.Plexi
         protected override void Update(GameTime gameTime)
         {
             _keyboardState = Keyboard.GetState();
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+          
+            if (CheckKey(Keys.Escape))
                 Exit();
 
-            _keyboardState = Keyboard.GetState();
-            if (_keyboardState.IsKeyDown(Keys.Escape))
-                Exit();
-
-            if(_keyboardState.GetPressedKeys().Contains(Keys.Back))
+            if (CheckKey(Keys.Back))
             {
-                _activeScreen.Hide();
-                _activeScreen = _startScreen;
-                _activeScreen.Show();
+                SetActiveScreen(_startScreen);
             }
 
             if (_activeScreen == _startScreen)
@@ -81,15 +70,11 @@ namespace Stixter.Plexi
                 {
                     if (_startScreen.SelectedIndex == 0)
                     {
-                        _activeScreen.Hide();
-                        _activeScreen = _actionScreen;
-                        _activeScreen.Show();
+                        SetActiveScreen(_actionScreen);
                     }
                     if (_startScreen.SelectedIndex == 1)
                     {
-                        _activeScreen.Hide();
-                        _activeScreen = _actionScreen;
-                        _activeScreen.Show();
+                        SetActiveScreen(_actionScreen);
                     }
                     if(_startScreen.SelectedIndex == 3)
                     {
@@ -97,14 +82,21 @@ namespace Stixter.Plexi
                     }
                 }
             }
+
             base.Update(gameTime);
             _oldKeyboardState = _keyboardState;
         }
 
+        private void SetActiveScreen(GameScreen gameScreen)
+        {
+            _activeScreen.Hide();
+            _activeScreen = gameScreen;
+            _activeScreen.Show();
+        }
+
         private bool CheckKey(Keys theKey)
         {
-            return _keyboardState.IsKeyUp(theKey) &&
-            _oldKeyboardState.IsKeyDown(theKey);
+            return _keyboardState.IsKeyUp(theKey) && _oldKeyboardState.IsKeyDown(theKey);
         }
 
         protected override void Draw(GameTime gameTime)
